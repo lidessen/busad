1e3e1035cc9c332640532bebfcbdc35294553aa7
ztytotoro <ztytotoro@outlook.com>
2022-08-21 15:50:19 +0800

    update

--- mod.ts
+++ mod.ts
@@ -1,3 +1,21 @@
+/** Build and Serve your app with Deno!
+ *
+ * this project is in a very early stage, you can try it by running `deno run -A https://deno.land/x/busad@0.1.0-alpha.1/demo.ts`, and use query `?branch=test` to switch branch
+ *
+ * ```ts
+ * import { busad } from "https://deno.land/x/busad@0.1.0-alpha.1/mod.ts";
+ *
+ * busad({
+ *   source: {
+ *     repo: "aoiste/deno_test_app",
+ *     entry: "main.tsx",
+ *   },
+ * }).serve();
+ * ```
+ *
+ * @module
+ */
+
 import * as esbuild from "https://deno.land/x/esbuild@v0.15.5/mod.js";
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.152.0/http/file_server.ts";
